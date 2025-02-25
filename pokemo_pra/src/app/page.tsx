a17178c7d0ab6a5c83e117f91ac8610990da456a
umekishimaru <riki12344649@gmail.com>
2025-02-25 18:17:30 +0900

    ポケモン日本語化対応

--- pokemo_pra/src/app/page.tsx
+++ pokemo_pra/src/app/page.tsx
@@ -2,30 +2,44 @@
 import { fetchAllPokemon } from "./pokemonApi/FetchAllPokemon";
 import { useEffect, useState } from "react";
 
+
+
+
+
+type Poke = {
+  id: number,
+  name: Promise<string>,
+  image: string,
+  types: {name: string}[]
+}
+
+
 export default function Home() {
-  const [pokemonList, setPokemoList] = useState<any[]>([]);
+  const [pokemonList, setPokemoList] = useState<Poke[]>([]);
   // const [loading, setLoading] = useState<boolean>(false);
 
   useEffect(() => {
     async function dataList() {
-      const pokemons: any = await fetchAllPokemon();
-      console.log(pokemons);
+      const pokemons: Poke[] = await fetchAllPokemon();
       setPokemoList(pokemons);
-      
     }
     dataList();
   }, []);
-  
+
   return (
     <>
       <h1>ポケモン図鑑</h1>
-
       <ul>
         {pokemonList.map((pokemon, index) => (
           <li key={index}>
+            <a href="#">
+            <p>No:{pokemon.id}</p>
             <img src={pokemon.image} alt={pokemon.name} />
-
             <p>{pokemon.name}</p>
+            {pokemon.types.map((type, typeIndex) => (
+              <p key={`${pokemon.name}-type-${typeIndex}`}>{type.name}</p>
+            ))}
+            </a>
           </li>
         ))}
       </ul>
--- pokemo_pra/src/app/pokemonApi/FetchAllPokemon.ts
+++ pokemo_pra/src/app/pokemonApi/FetchAllPokemon.ts
@@ -3,6 +3,47 @@ type Pokemon = {
   url: string;
 };
 
+const typeNamesList: { name: string; nameJa: string }[] = [
+  { name: "normal", nameJa: "ノーマル" },
+  { name: "fire", nameJa: "ほのお" },
+  { name: "water", nameJa: "みず" },
+  { name: "grass", nameJa: "くさ" },
+  { name: "electric", nameJa: "でんき" },
+  { name: "ice", nameJa: "こおり" },
+  { name: "fighting", nameJa: "かくとう" },
+  { name: "poison", nameJa: "どく" },
+  { name: "ground", nameJa: "じめん" },
+  { name: "flying", nameJa: "ひこう" },
+  { name: "psychic", nameJa: "エスパー" },
+  { name: "bug", nameJa: "むし" },
+  { name: "rock", nameJa: "いわ" },
+  { name: "ghost", nameJa: "ゴースト" },
+  { name: "dragon", nameJa: "ドラゴン" },
+  { name: "dark", nameJa: "あく" },
+  { name: "steel", nameJa: "はがね" },
+  { name: "fairy", nameJa: "フェアリー" },
+];
+
+type Poke = {
+  id: number;
+  name: Promise<string>;
+  image: string;
+  types: { name: string }[];
+};
+
+async function  changePokemonEnNameToJaName(enName: string): Promise<string> {
+
+const res =   await fetch(`https://pokeapi.co/api/v2/pokemon-species/${enName}`)
+
+const data = await res.json()
+const jaName : string  = data.names[0].name
+return jaName
+}
+
+function changeEnNameToJaName(enName: string) {
+  const typeJaName = typeNamesList.find((t) => t.name === enName);
+  return typeJaName?.nameJa;
+}
 
 export const fetchAllPokemon = async () => {
   try {
@@ -14,20 +55,27 @@ export const fetchAllPokemon = async () => {
     }
     const data = await res.json();
     const pokemons: Pokemon[] = data.results;
-    const pokemonList: any[] = [];
-// (async ()=>{
-  for await (const pokemon of pokemons){ 
-const res =    await fetch(pokemon.url)
-const pokemonData = await  res.json()
-const poke :{name: string,image:string} = {
-  name: pokemonData.name,
-  image: pokemonData?.sprites?.front_default,
-};
-pokemonList.push(poke)
-  }
-// })()
+    const pokemonList: Poke[] = [];
+
+    for await (const pokemon of pokemons) {
+      const res = await fetch(pokemon.url);
+      const pokemonData = await res.json();
+    
+      const poke: Poke = {
+        id: pokemonData.id,
+        name: changePokemonEnNameToJaName(pokemonData.name), 
+        image: pokemonData?.sprites?.front_default,
+        types: pokemonData?.types?.map((t: { type: { name: string } }) => ({
+          name: changeEnNameToJaName(t.type.name),
+        })),
+      };
+
+      pokemonList.push(poke);
+    }
+
     return pokemonList;
   } catch (error) {
     console.error("ポケモン情報取得エラー:", error);
+    return [];
   }
 };
